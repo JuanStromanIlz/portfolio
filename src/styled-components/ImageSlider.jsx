df6177107cee7431ec75e4f57c267d8c23f1f4d5
JuanStromanIlz <juanstroman@gmail.com>
2021-12-20 10:51:54 -0300

    new branch

--- src/styled-components/ImageSlider.jsx
+++ src/styled-components/ImageSlider.jsx
@@ -1,172 +1,151 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import Button from './Button';
+import { useState, useEffect } from "react";
+import {
+  Flex,
+  Modal,
+  Image,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  Box,
+  ModalFooter,
+  ModalHeader,
+  Text,
+  IconButton,
+  Breadcrumb,
+  BreadcrumbItem,
+  Fade,
+} from "@chakra-ui/react";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
-const Slider = styled.div`
-  display: none;
-  position: fixed;
-  inset: 0;
-  z-index: 11;
-  margin: 0;
-  background: ${props => props.theme.white};
-  justify-content: space-around;
-  align-items: center;
-  outline: none;
-  .imgContainer {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    .slider {
-      pointer-events: none;
-      opacity: 0;
-      position: absolute;
-      inset: 0;
-      display: flex;
-      margin: auto;
-      transition: .4s cubic-bezier(0.25, 0.46, 0.45, 0.94);
-      .sliderImg {
-        object-fit: scale-down;
-        width: 100%;
-        height: 100%;
-        padding: 1.6rem .8rem;
-        display: flex;
-        margin: auto;
+const GalleryModal = ({ open, images, index, onClose }) => {
+  const [galleryData, setGalleryData] = useState({
+    items: [],
+    index: 0,
+  });
+  const [touch, setTouch] = useState({
+    startPoint: 0,
+    direction: true,
+  });
+
+  function prevImage() {
+    setGalleryData((prev) => {
+      if (prev.index === 0) {
+        return { ...prev };
       }
-    }
-    .slider__show {
-      opacity: 1;
-      pointer-events: auto;
-    }
+      return {
+        items: prev.items,
+        index: prev.index - 1,
+      };
+    });
   }
-  .close {
-    z-index: 2;
-    position: absolute;
-    top: 1.6rem;
-    right: .8rem;
-  }
-  .list {
-    position: absolute;
-    bottom: 1.6rem;
-    left: 0;
-    right: 0;
-    padding-left: .8rem;
-    padding-right: .8rem;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    .buttons {
-      display: none;
-      flex-direction: row;
-      gap: .8rem;
-    }
-  }
-  @media(min-width: 920px) {
-    .close {
-      right: 3.2rem;
-    }
-    .imgContainer .slider .sliderImg {
-      padding: 1.6rem 3.2rem;
-      width: 70%;
-      height: 70%;
-    }
-    .list {
-      padding-left: 3.2rem;
-      padding-right: 3.2rem;
-      justify-content: space-between;
-      .buttons {
-        display: flex;
+
+  function nextImage() {
+    setGalleryData((prev) => {
+      let lastCard = prev.index + 1;
+      if (lastCard === prev.items.length) {
+        return { ...prev };
       }
-    }
+      return {
+        items: prev.items,
+        index: prev.index + 1,
+      };
+    });
   }
-`;
-
-
-const ImageSlider = ({images, detailImg, setDetailImg}) => {
-  const [touchStartPoint, setTouchStartPoint] = useState(0);
-  const [moveDirection, setMoveDirection] = useState(true);
 
   /* Touch */
   function touchStart(e) {
-    setTouchStartPoint(e.changedTouches[0].clientX);
+    setTouch((prev) => {
+      return {
+        startPoint: e.changedTouches[0].clientX,
+        direction: prev.direction,
+      };
+    });
   }
   function touchMove(e) {
-    setMoveDirection(e.changedTouches[0].clientX < touchStartPoint);
+    setTouch((prev) => {
+      return {
+        startPoint: prev.startPoint,
+        direction: e.changedTouches[0].clientX < prev.startPoint,
+      };
+    });
   }
   function touchEnd() {
-    let lastCard = detailImg + 1;
-    setTimeout(()=> {
-      if (moveDirection) {
-        if (lastCard < images.length) {
-          setDetailImg(detailImg +1);
-        }
-        if (lastCard === images.length) {
-          setDetailImg(0);
-        }
-      } else if(!moveDirection && detailImg > 0) {
-        setDetailImg(detailImg -1);
-      }
-    }, 250);
+    if (!touch.direction) {
+      return prevImage();
+    }
+    nextImage();
   }
 
   /* Keys */
   function keyPress(e) {
     if (e.keyCode === 39 || e.keyCode === 40) {
-      buttonRight();
+      nextImage();
     }
     if (e.keyCode === 37 || e.keyCode === 38) {
-      buttonLeft();
+      prevImage();
     }
   }
 
-  /* Buttons */
-  function buttonLeft() {
-    if (detailImg > 0) {
-      setDetailImg(detailImg -1);
+  useEffect(() => {
+    if (open) {
+      setGalleryData({
+        items: images,
+        index: index,
+      });
     }
-  }
-
-  function buttonRight() {
-    let lastCard = detailImg + 1;
-    if (lastCard < images.length) {
-      setDetailImg(detailImg +1);
-    }
-    if (lastCard === images.length) {
-      setDetailImg(0);
-    }
-  }
-
-  const closeSlider = () => {
-    document.getElementById('imageSlider').style.display='none';
-    document.body.removeAttribute('style');
-  }
+  }, [open, images, index]);
 
   return (
-    <Slider 
-      id='imageSlider'
-      onTouchStart={touchStart}
-      onTouchEnd={touchEnd}
-      onTouchMove={touchMove}
-      onKeyDown={keyPress}
-      tabIndex={0}
-    >
-      <Button className='close' onClick={closeSlider}>close</Button>
-      <div className='imgContainer'>
-        {images.map((img, i) => 
-          <div key={i} className={`slider  ${detailImg === i ? 'slider__show' : null}`}>
-            <img className='sliderImg' src={img} alt='detail' />
-          </div>
-        )}
-      </div>
-      <div className='list'>
-        <span>{`${detailImg + 1} / ${images.length}`}</span>
-        <div className='buttons'>
-          <Button onClick={buttonLeft}>prev</Button>
-          <Button onClick={buttonRight}>next</Button>
-        </div>
-      </div>
-    </Slider>
+    <Modal isOpen={open} onClose={onClose} size={"full"}>
+      <ModalContent
+        onKeyDown={keyPress}
+        onTouchStart={touchStart}
+        onTouchEnd={touchEnd}
+        onTouchMove={touchMove}
+        borderRadius={"none"}
+        bg={"gray.100"}
+      >
+        <ModalHeader>
+          <ModalCloseButton />
+        </ModalHeader>
+        <ModalBody pos={"relative"}>
+          {galleryData.items.map((src, index) => (
+            <Fade in={index === galleryData.index} key={index}>
+              <Box pos={"absolute"} left={0} top={0} right={0} bottom={0}>
+                <Flex height={"100%"} p={3}>
+                  <Image
+                    src={src}
+                    margin={"auto"}
+                    width={"100%"}
+                    maxWidth={["100%", "70vw"]}
+                    fit={"contain"}
+                  />
+                </Flex>
+              </Box>
+            </Fade>
+          ))}
+        </ModalBody>
+        <ModalFooter>
+          <Flex direction={"row"} justify={"space-between"} width={"100%"}>
+            <Flex direction={"column"} justifyContent={"center"}>
+              <Breadcrumb fontWeight={"bold"}>
+                <BreadcrumbItem>
+                  <Text>{galleryData.index + 1}</Text>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                  <Text>{galleryData.items.length}</Text>
+                </BreadcrumbItem>
+              </Breadcrumb>
+            </Flex>
+            <Flex direction={"row"}>
+              <IconButton onClick={prevImage} icon={<FaChevronLeft />} mr={1} />
+              <IconButton onClick={nextImage} icon={<FaChevronRight />} />
+            </Flex>
+          </Flex>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
-}
+};
 
-export default ImageSlider;
+export default GalleryModal;
